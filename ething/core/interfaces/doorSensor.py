2c718727936727c03ceed4b27b90af5046b01a26
Adrien Mezerette <amezerette@cls.fr>
2019-06-19 15:39:40 +0200

    store door state only on change

--- ething/core/interfaces/doorSensor.py
+++ ething/core/interfaces/doorSensor.py
@@ -5,7 +5,7 @@ from ..Interface import *
 
 
 @interface
-@attr('state', type = Boolean(), default = False, mode = READ_ONLY, history = True, force_watch = True, description = u"the state of the door. True if open.")
+@attr('state', type = Boolean(), default = False, mode = READ_ONLY, history = True, force_watch = False, description = u"the state of the door. True if open.")
 class DoorSensor(Sensor):
 
     def on_attr_update(self, attr, new_value, old_value):
