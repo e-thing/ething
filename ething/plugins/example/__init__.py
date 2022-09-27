60ba86bebf84cc00ceac84e7d53efb1fc8cd963a
amezerette <a.mezerette@gmail.com>
2022-09-27 12:40:41 +0200

    update ui

--- ething/plugins/example/__init__.py
+++ ething/plugins/example/__init__.py
@@ -2,11 +2,13 @@
 from ething.Device import *
 from ething.plugin import Plugin
 from ething.scheduler import set_interval, delay
-from ething.interfaces.thermometer import Thermometer
-from ething.interfaces.occupencySensor import OccupencySensor
+from ething.interfaces import *
 
+import os
 import time
 
+
+virtual_classes = []
 
 # note: the name of the plugin is given by the name of the directory and not the name of the plugin class.
 
@@ -38,25 +40,32 @@ class Example(Plugin):
         self.processes.add(self.background_process)
 
         # if no device has been yet created, create one :
-        if len(self.core.find(RandomThermometer)) == 0:
-            self.core.create(RandomThermometer, {
-                'name': 'random thermometer'
-            })
 
-        if len(self.core.find(RandomOccupencySensor)) == 0:
-            self.core.create(RandomOccupencySensor, {
-                'name': 'random occupency sensor'
-            })
+        for vcls in virtual_classes:
+            if len(self.core.find(vcls)) == 0:
+                self.core.create(vcls, {
+                    'name': vcls.__name__
+                })
 
-        if len(self.core.find(NotConnectedDevice)) == 0:
-            self.core.create(NotConnectedDevice, {
-                'name': 'not connected device'
-            })
-
-        if len(self.core.find(ErrorDevice)) == 0:
-            self.core.create(ErrorDevice, {
-                'name': 'device with error'
-            })
+        # if len(self.core.find(RandomThermometer)) == 0:
+        #     self.core.create(RandomThermometer, {
+        #         'name': 'random thermometer'
+        #     })
+        #
+        # if len(self.core.find(RandomOccupencySensor)) == 0:
+        #     self.core.create(RandomOccupencySensor, {
+        #         'name': 'random occupency sensor'
+        #     })
+        #
+        # if len(self.core.find(NotConnectedDevice)) == 0:
+        #     self.core.create(NotConnectedDevice, {
+        #         'name': 'not connected device'
+        #     })
+        #
+        # if len(self.core.find(ErrorDevice)) == 0:
+        #     self.core.create(ErrorDevice, {
+        #         'name': 'device with error'
+        #     })
 
         if len(self.core.find(lambda r: r.typeof('resources/File') and r.name == "example_file.txt")) == 0:
             f = self.core.create('resources/File', {
@@ -77,31 +86,14 @@ class Example(Plugin):
         self.logger.info('interval = %d' % self.interval)
 
 
-#
-# you may want to define some devices
-# -> a device may be a sensor, a light, a camera ... , the type of the device is given through inheritance
-#    generic interface are already defined in the interfaces directory.
-#    note: a device may inherit of multiple interfaces (thermometer + humiditySensor).
-# -> a device may have attributes (sensor value, light state ...)
-#    these attributes must be defined at the top of the class using the @attr decorator, see the interfaces/swith.py file.
-#    note: device class inherits from inherited interfaces.
-# -> a device may be controlled through methods (turn on, ...)
-#    a method is defined using @method decorator. See the interfaces/relay.py file.
-#
-class RandomThermometer (Thermometer):
-
-    # set_interval is a utility function that execute automatically a method periodically (see scheduler.py)
-    @set_interval(30)
-    def refresh(self):
-        self.temperature = random.randint(-10, 40)
 
 
 
-class RandomOccupencySensor (OccupencySensor):
 
-    @set_interval(30)
-    def refresh(self):
-        self.presence = bool(random.getrandbits(1))
+
+
+
+class VirtualWirelessDevice (Thermometer):
 
     @set_interval(60)
     def refresh_nat(self):
@@ -109,16 +101,152 @@ class RandomOccupencySensor (OccupencySensor):
         self.rlink_quality = random.randint(5, 100)
 
 
-class NotConnectedDevice (Thermometer):
+class VirtualNotConnectedDevice (Thermometer):
     @delay(1)
     def refresh(self):
         self.refresh_connect_state(False)
 
-class ErrorDevice (Thermometer):
+
+class VirtualErrorDevice (Thermometer):
     @delay(1)
     def refresh(self):
-        self.error = "<test> some error occured"
+        self.error = "This is a test. Some error occured"
 
 
 
+
+
+
+#
+# Button, Switch, ...
+#
+
+@meta(category="virtual")
+class VirtualButton(Button):
+
+    @set_interval(30)
+    def _main(self):
+        self.click()
+
+
+@meta(category="virtual")
+class VirtualDimmer(Dimmer):
+
+    @set_interval(30)
+    def _main(self):
+        self.level = int(random.random() * 100.)
+
+
+#
+# Actuators
+#
+
+@meta(category="virtual")
+class VirtualRelay(Relay):
+    pass
+
+
+@meta(category="virtual")
+class VirtualDimmableRelay(DimmableRelay):
+    pass
+
+
+@meta(category="virtual")
+class VirtualLight(Light):
+    pass
+
+
+@meta(category="virtual")
+class VirtualDimmableLight(DimmableLight):
+    pass
+
+
+@meta(category="virtual")
+class VirtualRGBWLight(RGBWLight):
+    pass
+
+
+
+
+#
+# Sensors
+#
+
+@meta(category="virtual")
+class VirtualThermometer(Thermometer):
+    @set_interval(30)
+    def _main(self):
+        self.temperature = round(random.random() * 30., 2)
+
+
+@meta(category="virtual")
+class VirtualPressureSensor(PressureSensor):
+    @set_interval(30)
+    def _main(self):
+        self.pressure = int(random.random() * 110000.)
+
+
+@meta(category="virtual")
+class VirtualHumiditySensor(HumiditySensor):
+    @set_interval(30)
+    def _main(self):
+        self.humidity = round(random.random() * 100., 1)
+
+
+@meta(category="virtual")
+class VirtualLightSensor(LightSensor):
+    @set_interval(30)
+    def _main(self):
+        self.light_level = round(random.random() * 800., 2)
+
+
+@meta(category="virtual")
+class VirtualMoistureSensor(MoistureSensor):
+    @set_interval(30)
+    def _main(self):
+        self.moisture = round(random.random() * 100., 1)
+
+
+@meta(category="virtual")
+class VirtualAnemometer(Anemometer):
+    @set_interval(30)
+    def _main(self):
+        self.wind_speed = round(random.random() * 40., 2)
+        self.wind_direction = int(random.random() * 360.)
+
+
+@meta(category="virtual")
+class VirtualDoorSensor(DoorSensor):
+    @set_interval(30)
+    def _main(self):
+        self.state = random.choice([True, False])
+
+
+@meta(category="virtual")
+class VirtualOccupencySensor (OccupencySensor):
+
+    @set_interval(30)
+    def refresh(self):
+        self.presence = bool(random.getrandbits(1))
+
+
+#
+# others
+#
+
+class VirtualCamera(Camera):
+
+    def snapshot(self):
+        with open(os.path.join(os.path.dirname(__file__), 'img.jpg'), mode='rb') as file:  # b is important -> binary
+            fileContent = file.read()
+        return fileContent
+
+
+
+
+
+for k in list(locals()):
+    v = locals()[k]
+    if k.startswith('Virtual') and issubclass(v, Resource):
+        virtual_classes.append(v)
 
